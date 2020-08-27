13bcb8b49d738edf537f6fbe2e622a591817c2b9
UmberBn <humberto-bandeira@hotmail.com>
2020-08-27 18:11:11 -0400

    exercise 7

---
+++ exercises/4_1/exercise7.js
@@ -0,0 +1,26 @@
+let porcentagem = -1;
+
+if(porcentagem > 0){
+    if(porcentagem>100){
+    return console.log("Calma lá meu camarada");
+    }
+    else if(porcentagem<50){
+      return console.log("F")
+    }
+    else if(porcentagem<60){
+        return console.log("E")
+    }
+    else if(porcentagem<70){
+        return console.log("D")
+    }
+    else if(porcentagem<80){
+        return console.log("C")
+    }
+    else if(porcentagem<90){
+        return console.log("B")
+    }
+    else if(porcentagem<=100){
+        return console.log("A")
+    } 
+}
+else(console.log("Foi tão ruim assim?"))
