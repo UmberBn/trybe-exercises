94acbc6cb22443e05d4c472d4322b902d060efdd
UmberBn <humberto-bandeira@hotmail.com>
2020-08-27 17:24:37 -0400

    exercicios de 1 a 5

---
+++ exercises/4_1/exercise4.js
@@ -0,0 +1,14 @@
+let a=0;
+
+
+if(a){
+    if(a>0){
+      return console.log("positive");
+    }
+    else if(a<0){
+        return console.log("negative");
+    }
+}
+else{
+    return console.log("zero");
+}
---
+++ exercises/4_1/exercise5.js
@@ -0,0 +1,12 @@
+let a = 60;
+let b = 60;
+let c = 60;
+
+let triangulo = a+b+c;
+
+if(triangulo){
+    if(triangulo == 180){
+        console.log("true")
+    }
+    else(console.log("não é um triangulo"))
+}
