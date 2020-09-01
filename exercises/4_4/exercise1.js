088d72350ba54f894e12655604f4c6fac17591a7
UmberBn <humberto-bandeira@hotmail.com>
2020-09-01 19:52:52 -0400

    First part of exercises

--- exercises/4_4/exercise1.js
+++ exercises/4_4/exercise1.js
@@ -2,8 +2,33 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  };
+};
+//exercise 1
+console.log("Bem vinda, " + info.personagem);
+console.log("-------------------------------")
+//exercise2
+info.recorrente = "Sim";
+console.log(info);
+//exercise3
+console.log("-------------------------------")
+for (let i in info) {
+    console.log(i);
+}
+console.log("-------------------------------")
+//exercise4
+for (let i in info) {
+    console.log(info[i]);
+}
+console.log("-------------------------------")
+//exercise5
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on bear mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+};
 
-  console.log("Bem vinda, "+info.personagem)
-
- 
+console.log(info.personagem + " e " + info2.personagem);
+console.log(info.origem + " e " + info2.origem);
+console.log(info.nota + " e " + info2.nota);
+console.log("Ambos Recorrentes");
